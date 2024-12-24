013a492e08fbd422cc6e54a260cef337927209d0
Joshua Sun <joshuapsun@gmail.com>
2024-12-23 20:07:22 -0500

    moved space partitioning code to it's own function

--- src/main.zig
+++ src/main.zig
@@ -54,65 +54,7 @@ pub fn main() !void {
                 }
                 pos.* = newPos;
             }
-            var arena = std.heap.ArenaAllocator.init(gpa.allocator());
-            defer arena.deinit();
-
-            var spacePartition = std.AutoHashMap(struct { i64, i64 }, std.ArrayList(u64)).init(arena.allocator());
-
-            for (0..circleArray.len) |i| {
-                const circ = circleArray.get(i);
-                const offsets = [_]rl.Vector2{ rl.Vector2.init(circ.radius, circ.radius), rl.Vector2.init(-circ.radius, circ.radius), rl.Vector2.init(circ.radius, -circ.radius), rl.Vector2.init(-circ.radius, -circ.radius) };
-                for (offsets) |offset| {
-                    const corner = circ.position.add(offset);
-                    const spacePartitionRegion: struct { i32, i32 } = .{ @intFromFloat(@floor(corner.x / 16)), @intFromFloat(@floor(corner.y / 16)) };
-                    var setOfCircles = try spacePartition.getOrPut(spacePartitionRegion);
-                    if (setOfCircles.found_existing) {
-                        try setOfCircles.value_ptr.append(i);
-                    } else {
-                        setOfCircles.value_ptr.* = std.ArrayList(u64).init(arena.allocator());
-                        try setOfCircles.value_ptr.append(i);
-                    }
-                }
-            }
-
-            var spacePartitionIter = spacePartition.iterator();
-            while (spacePartitionIter.next()) |entry| {
-                const setOfCircles = entry.value_ptr;
-                for (setOfCircles.items) |i| {
-                    for (setOfCircles.items) |j| {
-                        var circle1 = circleArray.get(i);
-                        var circle2 = circleArray.get(j);
-                        if (circle1.id == circle2.id) {
-                            continue;
-                        } else {
-                            const dist = circle1.position.distance(circle2.position);
-                            if (dist <= circle1.radius + circle2.radius) {
-                                const pos1 = circle1.position;
-                                const pos2 = circle2.position;
-                                const vel1 = circle1.velocity;
-                                const vel2 = circle2.velocity;
-                                const mass1 = circle1.mass;
-                                const mass2 = circle2.mass;
-                                const x1subx2: rl.Vector2 = pos1.subtract(pos2);
-                                const v1subv2: rl.Vector2 = vel1.subtract(vel2);
-                                const x2subx1: rl.Vector2 = pos2.subtract(pos1);
-                                const v2subv1: rl.Vector2 = vel2.subtract(vel1);
-                                const newVel1 = vel1.subtract(x1subx2.scale((2.0 * mass2 / (mass1 + mass2)) * (v1subv2.dotProduct(x1subx2) / x1subx2.lengthSqr())));
-                                const newVel2 = vel2.subtract(x2subx1.scale((2.0 * mass1 / (mass1 + mass2)) * (v2subv1.dotProduct(x2subx1) / x2subx1.lengthSqr())));
-                                circle1.velocity = newVel1;
-                                circle2.velocity = newVel2;
-                                const posOffset = dist - (circle1.radius + circle2.radius);
-                                const newPos1 = pos1.add(x2subx1.normalize().scale(posOffset / 2.0));
-                                const newPos2 = pos2.add(x1subx2.normalize().scale(posOffset / 2.0));
-                                circle1.position = newPos1;
-                                circle2.position = newPos2;
-                                circleArray.set(i, circle1);
-                                circleArray.set(j, circle2);
-                            }
-                        }
-                    }
-                }
-            }
+            try spacePartitionMethod(gpa.allocator(), &circleArray);
         }
         {
             rl.beginDrawing();
@@ -124,6 +66,68 @@ pub fn main() !void {
             var buf: [64]u8 = undefined;
             const formattedFrametime = try std.fmt.bufPrintZ(&buf, "{d} ms", .{rl.getFrameTime() * 1000});
             rl.drawText(formattedFrametime, 0, 0, 32, rl.Color.dark_gray);
+        }
+    }
+}
+
+fn spacePartitionMethod(allocator: std.mem.Allocator, circleArray: *std.MultiArrayList(Circle)) !void {
+    var arena = std.heap.ArenaAllocator.init(allocator);
+    defer arena.deinit();
+
+    var spacePartition = std.AutoHashMap(struct { i64, i64 }, std.ArrayList(u64)).init(arena.allocator());
+
+    for (0..circleArray.len) |i| {
+        const circ = circleArray.get(i);
+        const offsets = [_]rl.Vector2{ rl.Vector2.init(circ.radius, circ.radius), rl.Vector2.init(-circ.radius, circ.radius), rl.Vector2.init(circ.radius, -circ.radius), rl.Vector2.init(-circ.radius, -circ.radius) };
+        for (offsets) |offset| {
+            const corner = circ.position.add(offset);
+            const spacePartitionRegion: struct { i32, i32 } = .{ @intFromFloat(@floor(corner.x / 16)), @intFromFloat(@floor(corner.y / 16)) };
+            var setOfCircles = try spacePartition.getOrPut(spacePartitionRegion);
+            if (setOfCircles.found_existing) {
+                try setOfCircles.value_ptr.append(i);
+            } else {
+                setOfCircles.value_ptr.* = std.ArrayList(u64).init(arena.allocator());
+                try setOfCircles.value_ptr.append(i);
+            }
+        }
+    }
+
+    var spacePartitionIter = spacePartition.iterator();
+    while (spacePartitionIter.next()) |entry| {
+        const setOfCircles = entry.value_ptr;
+        for (setOfCircles.items) |i| {
+            for (setOfCircles.items) |j| {
+                var circle1 = circleArray.get(i);
+                var circle2 = circleArray.get(j);
+                if (circle1.id == circle2.id) {
+                    continue;
+                } else {
+                    const dist = circle1.position.distance(circle2.position);
+                    if (dist <= circle1.radius + circle2.radius) {
+                        const pos1 = circle1.position;
+                        const pos2 = circle2.position;
+                        const vel1 = circle1.velocity;
+                        const vel2 = circle2.velocity;
+                        const mass1 = circle1.mass;
+                        const mass2 = circle2.mass;
+                        const x1subx2: rl.Vector2 = pos1.subtract(pos2);
+                        const v1subv2: rl.Vector2 = vel1.subtract(vel2);
+                        const x2subx1: rl.Vector2 = pos2.subtract(pos1);
+                        const v2subv1: rl.Vector2 = vel2.subtract(vel1);
+                        const newVel1 = vel1.subtract(x1subx2.scale((2.0 * mass2 / (mass1 + mass2)) * (v1subv2.dotProduct(x1subx2) / x1subx2.lengthSqr())));
+                        const newVel2 = vel2.subtract(x2subx1.scale((2.0 * mass1 / (mass1 + mass2)) * (v2subv1.dotProduct(x2subx1) / x2subx1.lengthSqr())));
+                        circle1.velocity = newVel1;
+                        circle2.velocity = newVel2;
+                        const posOffset = dist - (circle1.radius + circle2.radius);
+                        const newPos1 = pos1.add(x2subx1.normalize().scale(posOffset / 2.0));
+                        const newPos2 = pos2.add(x1subx2.normalize().scale(posOffset / 2.0));
+                        circle1.position = newPos1;
+                        circle2.position = newPos2;
+                        circleArray.set(i, circle1);
+                        circleArray.set(j, circle2);
+                    }
+                }
+            }
         }
     }
 }
